df2ebb71583387e90d695254761564643047ac15
Carl Messing <c.messing@campus.tu-berlin.de>
2025-06-12 20:13:52 +0200

    moving ip and port to env

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use warp::Filter;
 use std::convert::Infallible;
+use std::env;
+use std::net::{Ipv4Addr, SocketAddr};
 use crate::endpoints::greet;
 use crate::handlers::reply_notfound;
 
@@ -8,6 +10,24 @@ mod handlers;
 mod utils;
 
 async fn handle_notfound() -> Result<impl warp::Reply, Infallible> { Ok(reply_notfound()) }
+
+async fn env_ip() -> [u8; 4] {
+    let ip_str = env::var("IP_ADDRESS").unwrap_or_else(|_| "127.0.0.1".to_string());
+
+    let ip: Ipv4Addr = ip_str.parse().unwrap_or_else(|_| {
+        Ipv4Addr::new(127, 0, 0, 1)
+    });
+    // [ip.octets()[0] as i32, ip.octets()[1] as i32, ip.octets()[2] as i32, ip.octets()[3] as i32]
+    ip.octets()
+}
+
+async fn env_port() -> u16 {
+    let my_value: i32 = env::var("MY_INT")
+        .ok()
+        .and_then(|val| val.parse::<i32>().ok())
+        .unwrap_or(3030);
+    my_value as u16
+}
 
 #[tokio::main]
 async fn main() {
@@ -43,10 +63,10 @@ async fn main() {
         .or(notfound);
 
     // Start server
-    let ip = [127, 0, 0, 1];
-    let port = 3030;
+    let ip = env_ip().await;
+    let port = env_port().await;
     println!("✅ Server is running on {}.{}.{}.{}:{}.", ip[0], ip[1], ip[2], ip[3], port);
     warp::serve(routes)
-        .run((ip, port))
+        .run(SocketAddr::from((ip, port)))
         .await;
 }
